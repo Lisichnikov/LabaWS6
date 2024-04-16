7d73f66ebee370939c00c3bee62bf61e855abe6d
Алексей Лисичников <alexochanel@gmail.com>
2024-04-16 12:47:59 +0300

    Добавьте файлы проекта.

---
+++ LabaWS6/Ship.cs
@@ -0,0 +1,13 @@
+﻿abstract class Ship : Transport
+{
+    public override string GetInfo()
+    {
+        return $"Корабль со скоростью {Speed} узлов, грузоподъемностью {CarryingCapacity} килограмм, может пройти {Range} морских миль.";
+    }
+}
+class Yacht : Ship
+{
+    public override int Speed => 23;
+    public override int CarryingCapacity => 500;
+    public override int Range => 12000;
+}
